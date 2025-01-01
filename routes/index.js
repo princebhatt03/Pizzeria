d6c7a7bb8619df544b96be5e4ddc8f453d92112f
Prince Bhatt <sharmaprince89667@gmail.com>
2025-01-01 18:51:07 +0530

    6th Commit, Login, Register Page Done

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,12 @@ router.get('/cart', function (req, res, next) {
   res.render('customer/cart');
 });
 
+router.get('/login', function (req, res, next) {
+  res.render('auth/login');
+});
+
+router.get('/reg', function (req, res, next) {
+  res.render('auth/register');
+});
+
 module.exports = router;
